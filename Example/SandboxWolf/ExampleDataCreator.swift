049016de6fce4238eec0eab537dbb75983c6b463
Marcel <hagmann.marcel@yahoo.com>
2020-05-23 14:41:43 +0200

    Update SandboxWolf example project

--- Example/SandboxWolf/ExampleDataCreator.swift
+++ Example/SandboxWolf/ExampleDataCreator.swift
@@ -1,9 +1,26 @@
 //
 //  ExampleDataCreator.swift
-//  SandboxWolf_Example
+//  SandboxManager_Example
 //
 //  Created by Marcel Hagmann on 23.05.20.
 //  Copyright © 2020 CocoaPods. All rights reserved.
 //
 
 import Foundation
+
+class ExampleDataCreator {
+    
+    static func create10HelloWorldFilesInDocumentDirectory() {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        let documentsDirectory = paths[0]
+        
+        for index in 1...10 {
+            let fileName = "Hello World \(index)"
+            let data = "Hello Content \(index)".data(using: .utf8)
+            
+            let destinationURL = documentsDirectory.appendingPathComponent(fileName)
+            try? data?.write(to: destinationURL)
+        }
+    }
+    
+}
--- Example/SandboxWolf/ViewController.swift
+++ Example/SandboxWolf/ViewController.swift
@@ -1,24 +1,38 @@
 //
 //  ViewController.swift
-//  SandboxWolf
+//  SandboxManager
 //
 //  Created by Marcel on 05/23/2020.
 //  Copyright (c) 2020 Marcel. All rights reserved.
 //
 
 import UIKit
+import SandboxWolf
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction private func createExampleDataAction() {
+        ExampleDataCreator.create10HelloWorldFilesInDocumentDirectory()
+    }
+    
+    @IBAction private func openSandboxManagerAction() {
+        let vctrl = SandboxManagerViewController(roots: [
+            .sandbox,
+            // Add your own app group identifier if you want to move
+            // files between the sandbox and the app group
+            // .appGroup(applicationGroupIdentifier: "your-appgroup-id")
+            ]
+        )
+        self.present(vctrl, animated: true)
+    }
 
 }
-
